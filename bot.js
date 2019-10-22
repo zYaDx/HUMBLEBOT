2310aa698f71ba77c8935763ce690eaf658622cc
zYaDx <45242928+zYaDx@users.noreply.github.com>
2019-10-22 14:33:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "#";
+var prefix = "-";
 
 
 client.on('message', message => {
@@ -583,6 +583,79 @@ client.on('channelDelete',channel =>{
     }, temp[neww.guild.id].time);}
 })
 });
+
+
+
+
+
+
+const developers = ["283880597095710721","338838856638136331"]
+
+client.on('message', message => {
+
+    var argresult = message.content.split(` `).slice(1).join(' ');
+
+      if (!developers.includes(message.author.id)) return;
+
+      
+
+  if (message.content.startsWith(adminprefix + 'setg')) {
+
+    client.user.setGame(argresult);
+
+      message.channel.send(`**✅   ${argresult}**`)
+
+  } else 
+
+     if (message.content === (adminprefix + "leavellll")) {
+
+    message.guild.leave();        
+
+  } else  
+
+  if (message.content.startsWith(adminprefix + 'setw')) {
+
+  client.user.setActivity(argresult, {type:'WATCHING'});
+
+      message.channel.send(`**✅   ${argresult}**`)
+
+  } else 
+
+  if (message.content.startsWith(adminprefix + 'setl')) {
+
+  client.user.setActivity(argresult , {type:'LISTENING'});
+
+      message.channel.send(`**✅   ${argresult}**`)
+
+  } else 
+
+  if (message.content.startsWith(adminprefix + 'sets')) {
+
+    client.user.setGame(argresult, "https://www.twitch.tv/zyod4");
+
+      message.channel.send(`**✅**`)
+
+  }
+
+  if (message.content.startsWith(adminprefix + 'setname')) {
+
+  client.user.setUsername(argresult).then
+
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+
+} else
+
+if (message.content.startsWith(adminprefix + 'setava')) {
+
+  client.user.setAvatar(argresult);
+
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+
+}
+
+});
+
+
 
 
 
